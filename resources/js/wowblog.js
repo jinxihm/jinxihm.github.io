3aaa5cb997ddc177cf8833883d6850212ba1b2d7
jinxihm <jinxigit@sina.com>
2014-12-01 00:48:24 +0800

    Update wowblog.js

--- resources/js/wowblog.js
+++ resources/js/wowblog.js
@@ -1,28 +1,43 @@
 $(document).ready(function() {
-
-	var getCategory = function(){
+	
+	window.wowblog = {};
+	wowblog.category = {};
+	wowblog.pageInfo = {};
+	// 获取目录信息
+	(function(){
 		$.getJSON("category.json", function(data){
-			// 创建global variable
-			window.category = data; 
+			wowblog.category = data; 
 		});
-	}
-	getCategory();
-
-	function isNewBlog(){
+	})();
+	
+	
+	
+	function isNewPage(){
 		return (location.href.indexOf('newblog')>-1);
 	}
 
-	// 新建博客
-	if(isNewBlog()){
+	// 新建页面
+	if(isNewPage()){
 		$('#blogTitle').attr('contentEditable',true).css('min-height',"24px").focus();
 		$('#blogContent').attr('contentEditable',true).css('min-height',"300px");
 		$('#edit').hide();
 		$('#cancel').hide();
 		$('#save').show();
+		$('#categoryDropMenu').text("请选择分类");
+	// 直接打开已存在页面
+	}else{
+		// 获取页面信息
+		(function(){
+			getContent(location.pathname,function(data){
+				wowblog.pageInfo.path = data.path;
+				wowblog.pageInfo.sha = data.sha;
+			},function(data){
+				console.log("获取页面信息失败:");
+				console.log(data);
+			});
+		})();
 	}
 	
-	var urlPre = "https://api.github.com/repos/jinxihm/jinxihm.github.io/contents/";
-	var token = "?access_token=41228f8e62cef808a68215e6a7b90296465b3db9";
 	
 	// 编辑
 	$('#edit').click(function() {
@@ -58,27 +73,38 @@ $(document).ready(function() {
 	});
 	// 保存
 	$('#save').click(function() {
+		// 检查
+		if($('#blogTitle').text().length == 0){
+			alert('请输入标题');
+			return;
+		}
+		if($('#categoryDropMenu').text() === "请选择分类"){
+			alert('请选择分类');
+			return;
+		}
+		
 		// 修改页面元素状态
-		$('#blogTitle').attr('contentEditable',false);
-		$('#blogContent').attr('contentEditable',false);
+		$('#blogTitle').attr('contentEditable',false).css('background-color','transparent');
+		$('#blogContent').attr('contentEditable',false).css('background-color','transparent');
 		$('#toobBar').hide();
 		$('#save').hide();
 		$('#cancel').hide();
-		$('#info').html("正在保存,请稍等...");
 		
-		var path =  blogInfo.path || (new Date()).getTime() + ".html";
-		
+		if(isNewPage()){
+			path = "/" + $('#categoryDropMenu').text() + "/" + (new Date()).getTime() + ".html";
+		}
+				
 		// 整个html文档,编码成base64.
 		var content = "<!DOCTYPE html><html>" + $('html').html() + "</html>";
 		
 		saveFile(path, content, blogInfo.sha, saveFileSucceed, saveFileFailed);
 		
+		$('#info').html("正在保存,请稍等...");
+		
 		function saveFileSucceed(data){
 			// 更新blogInfo对象
-			blogInfo.path = data.content.path;
-			blogInfo.sha = data.content.sha;
-			blogInfo.url = data.content.url;
-			blogInfo.html_url = data.content.html_url;
+			wowblog.pageInfo.path = data.content.path;
+			wowblog.pageInfo.sha = data.content.sha;
 			
 			// 更改地址栏
 			var stateObject = {};
@@ -87,8 +113,8 @@ $(document).ready(function() {
 			history.pushState(stateObject,title,newUrl);
 			
 			window.addEventListener('popstate', function(event) {
-				debugger;  
-				alert(event);
+				//debugger;  
+				//alert(event);
 				});
 			
 			
@@ -113,19 +139,26 @@ $(document).ready(function() {
 		$('#contentNavArea').show();
 		$('#contentNavArea').siblings().hide();
 		
-	})
+	});
+	$('#contentCategoryBtn').click(function(){
+		$('#contentCategoryBtn').addClass("sel");
+		$('#contentCategoryBtn').siblings().removeClass("sel");
+		$('#contentCategoryArea').show();
+		$('#contentCategoryArea').siblings().hide();
+		
+	});
 	$('#contentNoteBtn').click(function(){
 		$('#contentNoteBtn').addClass("sel");
 		$('#contentNoteBtn').siblings().removeClass("sel");
 		$('#contentNoteArea').show();
 		$('#contentNoteArea').siblings().hide();
-	})
+	});
 	$('#contentRefBtn').click(function(){
 		$('#contentRefBtn').addClass("sel");
 		$('#contentRefBtn').siblings().removeClass("sel");
 		$('#contentRefArea').show();
 		$('#contentRefArea').siblings().hide();
-	})
+	});
 	
 	
 	
@@ -133,7 +166,9 @@ $(document).ready(function() {
 		alert('正在完善中...');
 	});
 	
-	
+	$('#contentCategoryArea .dropdown a').click(function(){
+		$('#categoryDropMenu').text($(this).text());
+	});
 	
 	
 	
