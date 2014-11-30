857f9cacac2833720afa9e63d9b0eae4198cb859
jinxihm <jinxigit@sina.com>
2014-12-01 00:48:03 +0800

    Update githubapi.js

--- resources/js/githubapi.js
+++ resources/js/githubapi.js
@@ -1,41 +1,69 @@
 /* url for repositories API*/
-var reposUrl = "https://api.github.com/repos/jinxihm/jinxihm.github.io/contents/";
-var token = "?access_token=41228f8e62cef808a68215e6a7b90296465b3db9"
-	
-var getFileContent = function getFileContent(path){
-	
-}
+var reposUrl = "https://api.github.com/repos/jinxihm/jinxihm.github.io/contents";
+var sitePagesPath = "/sitepages";
+var token = "?access_token=41228f8e62cef808a68215e6a7b90296465b3db9";
 
-var getDirContent = function getDirContent(dirPath, successFunc, failFunc){
+
+
+var getContent = function getContent(path, successFunc, failFunc) {
 	$.ajax({
-		type: "get",
-		url: reposUrl + dirPath + token,
+		type : "get",
+		url : reposUrl + path + token,
 		dataType : "json",
-		success: successFunc,
-		error: failFunc
+		success : successFunc,
+		error : failFunc
 	});
-}
-
+};
 /**
  * 保存或更新文件。文件夹需要通过创建文件间接创建。
  */
-var saveFile = function saveFile(path, fileContent, sha ,successFunc, failFunc){
+var saveFile = function saveFile(path, fileContent, sha, successFunc, failFunc) {
 	var inputData = {};
 	inputData.message = "my commit message";
-	inputData.committer = {	"name" : "n1",	"email" : "n2@gmail.com" };
+	inputData.committer = {
+		"name" : "n1",
+		"email" : "n2@gmail.com"
+	};
 	// base64编码，参见mdn
 	inputData.content = window.btoa(unescape(encodeURIComponent(fileContent)));
 	// 有sha参数表明是更新文件，否则为创建文件
-	if(sha !== null){
+	if (sha !== null) {
 		inputData.sha = sha;
 	}
 	$.ajax({
 		type : "put",
-		url :   reposUrl + path+ token,
+		url : reposUrl + sitePagesPath + path + token,
 		contentType : "application/json",
-		data : JSON.stringify(inputData),			
+		data : JSON.stringify(inputData),
 		dataType : "json",
-		success: successFunc,
-		error: failFunc
-	})
-}
+		success : successFunc,
+		error : failFunc
+	});
+};
+
+
+var createDir = function createDir(path, successFunc, failFunc){
+	//var dirIndex = {pageNum:2,pages:[{titel:"title", path: "path"}]}
+	
+	path = path + "/dirIndex.json";
+	var fileContent ="{}";
+	saveFile(path, fileContent, null, successFunc, failFunc);
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
